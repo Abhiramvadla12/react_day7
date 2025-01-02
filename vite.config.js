0e2917cb950a06057ffa0b560a8a5758fb4364ee
Abhiramvadla12 <abhiramvadla61@gmail.com>
2025-01-02 19:25:23 +0530

    version 5

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        { src: 'index.html', dest: '' }
-      ]
-    })
-  ],
-  base: '/<react_day7>/',
+  plugins: [react()],
+  base: '/react_day7/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+  server: {
+    historyApiFallback: true,
+  },
 });
