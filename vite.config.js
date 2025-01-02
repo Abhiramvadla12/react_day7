a062bf53d9deb897d2cad532f8824758b101889c
Abhiramvadla12 <abhiramvadla61@gmail.com>
2025-01-02 19:13:56 +0530

    version 4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/<react_day7>/', // Replace <repository-name> with your repo name
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'index.html', dest: '' }
+      ]
+    })
+  ],
+  base: '/<react_day7>/',
 });
