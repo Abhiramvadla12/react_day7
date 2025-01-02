08b57afccd66b1d51201d2cf4f9c2d22c187bc1e
Abhiramvadla12 <abhiramvadla61@gmail.com>
2025-01-02 18:41:29 +0530

    version 4

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<repository-name>/', // Replace <repository-name> with your repo name
+  base: '/<react_day7>/', // Replace <repository-name> with your repo name
 });
